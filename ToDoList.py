bbcc47a7b8fe2b52ba7b2b91193e6b53eec53663
Murat ARABACI <157636614+muraatarabaci@users.noreply.github.com>
2025-10-22 05:54:30 +0300

    ToDoList.py

--- ToDoList.py
+++ ToDoList.py
@@ -2,21 +2,18 @@ import os
 
 dosya = "gorevler.txt"
 
-# Görevleri dosyadan okuyoruz. Dosya yoksa boş liste dönüyor.
 def yukle():
     if os.path.exists(dosya):
         with open(dosya, "r", encoding="utf-8") as f:
             return [x.strip() for x in f]
     print("Dosya bulunamadı. Yeni liste oluşturuluyor.\n")
     return []
-
-# Görevleri dosyaya kaydediyoruz.
+    
 def kaydet(gorevler):
     with open(dosya, "w", encoding="utf-8") as f:
         f.write("\n".join(gorevler))
     print("Görevler kaydedildi.\n")
 
-# Görevleri ekranda liste halinde gösteriyoruz.
 def listele(gorevler):
     if not gorevler:
         print("Henüz görev yok.\n")
@@ -25,7 +22,6 @@ def listele(gorevler):
             print(f"{i}. {g}")
         print()
 
-# Yeni görev ekliyoruz, boş girilirse uyarı veriyoruz.
 def ekle(gorevler):
     while True:
         g = input("Yeni görev: ").strip()
@@ -37,7 +33,6 @@ def ekle(gorevler):
         else:
             print("Görev boş olamaz! Lütfen bir görev girin.")
 
-# Mevcut görevlerden birini düzenliyoruz.
 def duzenle(gorevler):
     listele(gorevler)
     try:
@@ -52,7 +47,6 @@ def duzenle(gorevler):
     except:
         print("Lütfen geçerli bir sayı girin.\n")
 
-# Seçilen görevi siliyoruz.
 def sil(gorevler):
     listele(gorevler)
     try:
@@ -65,7 +59,6 @@ def sil(gorevler):
     except:
         print("Lütfen geçerli bir sayı girin.\n")
 
-# Kullanıcı tüm görevleri silmek isterse bu kod satırı çalışıyor ve en son yanlışlıkla silinmemesi için kullanıcıdan onay alıyoruz.
 def tumunu_sil(gorevler):
     cevap = input("Tüm görevleri silmek istediğinize emin misiniz? (e/h): ").lower()
     if cevap == "e":
@@ -75,7 +68,6 @@ def tumunu_sil(gorevler):
     else:
         print("İşlem iptal edildi.\n")
 
-# Kullanıcıya menüdeki seçenekleri gösteriyoruz.
 def menu():
     print("1. Görevleri Göster")
     print("2. Yeni Görev Ekle")
@@ -84,19 +76,16 @@ def menu():
     print("5. Tüm Görevleri Sil")
     print("6. Çık\n")
 
-# Programın karşılama ekranında burası çalışıyor.
 def hosgeldin():
     print("To-Do List Uygulamasına Hoş geldiniz.\n")
     g_list = yukle()
 
-    # Program açıldığında görev varsa hemen gösteriyoruz.
     if g_list:
         print("Kayıtlı görevleriniz:\n")
         listele(g_list)
     else:
         print("Herhangi bir kayıtlı görev bulunamadı.\n")
 
-    # Kullanıcı çıkmak isteyene kadar menüyü döngüde gösteriyoruz.
     while True:
         menu()
         secim = input("Seçiminiz (1-6): ").strip()
@@ -117,6 +106,6 @@ def hosgeldin():
         else:
             print("Geçersiz giriş.\n")
 
-# Programı başlatan kısım.
 if __name__ == "__main__":
     hosgeldin()
+
